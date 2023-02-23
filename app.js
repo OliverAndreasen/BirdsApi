bd7affeeb6f18401112fcfe68e98e180ed107d96
Oliver <oliv2058@stud.kea.dk>
2023-02-23 20:46:48 +0100

    init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 //instantiate
 const app = express();
 
-//app.use(express.json());
+app.use(express.json());
 
 const birds = [
     { id: 1, name: "Pigeon" },
@@ -25,7 +25,16 @@ app.get("/birds", (req, res) => {
 
 app.get("/birds/:Id", (req, res) => {
     const id = req.params.Id;
+    if (isNaN(id)) {
+        return res.send(({message: "Id must be a number"}));
+    }
+
     const bird = birds.find(bird => bird.id === parseInt(id));
+
+    if(bird === undefined) {
+        return res.send(({message: "Bird not found"}));
+    }
+
     res.send({bird});
 });
 
@@ -33,6 +42,58 @@ app.get("/birds/name/:Name", (req, res) => {
     const name = req.params.Name;
     const bird = birds.find(bird => bird.name === (name));
     res.send({ "data": bird});
+});
+
+app.post("/birds", (req, res) => {
+    const lastId = birds[birds.length - 1].id;
+
+    const newBird = {
+        id: lastId + 1,
+        name: req.body.name
+    };
+
+    birds.push(newBird);
+    res.send({
+        "message": "Bird added successfully",
+        "data": birds});
+});
+
+app.patch("/birds/:Id", (req, res) => {
+    const id = req.params.Id;
+
+    if (isNaN(id)) {
+        return res.send(({message: "Id must be a number"}));
+    }
+
+    const bird = birds.find(bird => bird.id === parseInt(id));
+    if(bird === undefined) {
+        return res.send(({message: "Bird not found"}));
+    }
+
+    bird.name = req.body.name;
+    res.send({
+        "message": "Bird updated successfully",
+        "data": birds});
+});
+
+app.delete("/birds/:Id", (req, res) => {
+    const id = req.params.Id;
+
+    if (isNaN(id)) {
+        return res.send(({message: "Id must be a number"}));
+    }
+
+    const bird = birds.find(bird => bird.id === parseInt(id));
+
+    if(bird === undefined) {
+        return res.send(({message: "Bird not found"}));
+    }
+
+    const index = birds.indexOf(bird);
+    birds.splice(index, 1);
+    res.send({
+        "message": "Bird deleted successfully",
+        "data": birds});
 });
 
 
